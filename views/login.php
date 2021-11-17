b76e5fc2073df59545ca43be5d8465593ef4bf2e
Reyna <al060872@uacam.mx>
2021-11-16 23:58:20 -0600

    se añadio las conexiones a la bd

--- views/login.php
+++ views/login.php
@@ -1,3 +1,22 @@
-<form action="">
-   
-</form>
+<div class="container">
+
+        <div class="columns is-centered m-6">
+            <form class="column is-half content is-normal p-6" action="" method="post">
+                <h1>Iniciar sesión</h1>
+
+                <div class="field">
+                    <label class="label">Nombre de usuario</label>
+                    <div class="control">
+                        <input class="input" type="text" name="usuario" id="" style="text-transform: capitalize;"
+                            placeholder="Anacleto" >
+                    </div>
+                    <div class="control">
+                        <button class="button is-link mt-4" type="submit">Enviar</button>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+
+    </div>
+</div>
