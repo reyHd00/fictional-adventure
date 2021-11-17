155f6fe29b8eac2933e5671be5ac335449b2f772
Reyna <al060872@uacam.mx>
2021-11-16 19:40:10 -0600

    Se añadieron las ventanas principales y las clases de la pagina web

--- controller/routing.php
+++ controller/routing.php
@@ -13,7 +13,7 @@ switch ($var_getMenu) {
             require_once('./views/login.php');
         break;
     case "registrarse":
-            require_once('./views/singin.php');
+            require_once('./views/registrarse.php');
         break;
     case "carrusel":
             require_once('./views/carrusel.php');
@@ -29,25 +29,6 @@ switch ($var_getMenu) {
     case "logout":
             $session_destroy = session_destroy();
             header("location: ./index.php?menu=home");
-        break;
-    case "deletealumno":
-            $_idalumno = trim(filter_input(INPUT_GET, 'idalumno'));
-            // $_idalumno = isset($_GET['idalumno']) ? $_GET['idalumno'] : '0';
-            require_once ('./models/alumno.php');
-            $sqlAlumno = Alumno::delete($_idalumno);
-            header("location: ./index.php?menu=alumno");
-        break;
-        case "editalumno":
-            $_idalumno = trim(filter_input(INPUT_GET, 'idalumno'));
-            require_once ('./models/alumno.php');
-            $sqlAlumno = Alumno::consultarAlumno($_idalumno);
-            include_once './views/editar.php';
-        break;
-        case "editar":
-            require_once('./views/editar.php');
-        break; 
-        case "insertalumno":
-            require_once('./views/inserAlumno.php');
         break;
     default:
         require_once('./views/home.php');
--- views/navegador.php
+++ views/navegador.php
@@ -1,7 +1,34 @@
-<nav>
-    <ul>
-        <li><a href=""></a>Inicio</li>
-        <li><a href=""></a>Acerca de</li>
-        <li><a href="?menu=login"></a>Iniciar Sesión</li>
-    </ul>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand ml-3">
+        <a class="navbar-item" href="index.php">
+            <img src="views/img/brand_logo.png" width="30" height="48">
+        </a>
+
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+            <a class="navbar-item" href="?menu=home">
+                Home
+            </a>
+
+            <!-- <a class="navbar-item">
+                Documentation
+            </a> -->
+
+        </div>
+
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a class="button is-primary" href="?menu=registrarse">
+                        <strong>Sign up</strong>
+                    </a>
+                    <a class="button is-light" href="?menu=login">
+                        Log in
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </nav>
